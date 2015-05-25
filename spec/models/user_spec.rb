73cc23dc9ef8981069b1ff32bcc9a1f48d51bebf
Solli86 <paul-sart@yandex.ru>
2015-05-25 20:50:20 +0300

    valid name/email non empty

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require 'spec_helper'
+describe User do
+  before  { @user = User.new( name: "Solli_86", email: "solli@mix.up") }
+  subject { @user }
+  it { should respond_to(:name)  }
+  it { should respond_to(:email) }
+  it  {should be_valid }
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "When name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+  describe "when email is not present" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
 end
