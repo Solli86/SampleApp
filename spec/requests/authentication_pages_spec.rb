3793642e689c5facc5896f74b3df26e909cc0b19
Solli86 <paul-sart@yandex.ru>
2015-06-02 16:45:35 +0300

    fail test to sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,7 +6,11 @@ describe "Authentication" do
   describe "sign in page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title("Sign in") }
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+      
+      it { should have_title("Sign in") }
+      it { should have_selector('.alert-danger') }
+    end
   end
 end
