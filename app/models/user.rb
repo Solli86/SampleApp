961fab779d924a2d20606bcea5f2e38164473a89
Solli86 <paul-sart@yandex.ru>
2015-06-02 14:55:05 +0300

    finish the lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ActiveRecord::Base
   validates(
     :name,    
     presence: true,
-    length: { maximum: 50 }
+    length: { minimum: 3, maximum: 50 }
   )
   # Email validation
   VALID_EMAIL_REGEX = /[a-z\d\+-.]+@[a-z\d\-]+[.]{1}[a-z]+\z/i  
   validates(
     :email,  
     presence: true,
+    length: {minimum: 8, maximum: 255},
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
   )
