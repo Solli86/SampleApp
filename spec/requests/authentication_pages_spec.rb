9e7d124629c3cf05c13d56a5784b43c525eac237
Solli86 <paul-sart@yandex.ru>
2015-06-03 22:18:16 +0300

    ready test to login form

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,5 +12,18 @@ describe "Authentication" do
       it { should have_title("Sign in") }
       it { should have_selector('.alert-danger') }
     end
+    describe "with valid sign in" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "email",    with:  user.email.upcase
+        fill_in "password", with: user.password
+        click_button "Sign_in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',      href: user_path(user)) }
+      it { should have_link('Sign out',     href: signout_path) }
+      it { should_not have_link('Sign in',  href: signin_path) }
+    end
   end
 end
