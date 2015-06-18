fd34d263572a8e660f85060ca15ce750a6c0f93f
Solli86 <paul-sart@yandex.ru>
2015-06-18 22:04:48 +0300

    temp -- almost complete edit test

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -57,5 +57,22 @@ describe "User page" do
       before { click_button "Save changes" }
       it { should have_content('error') }
     end
+    describe "with valid information" do
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "name",             with: new_name
+        fill_in "email",            with: new_email
+        fill_in "password",         with: user.password
+        fill_in "confirm the password", with: user.password
+        click_button "Save changes"
+      end
+
+      it { should have_title(new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { expect(user.reload.name).to  eq new_name }
+      specify { expect(user.reload.email).to eq new_email }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-
+  Capybara.ignore_hidden_elements = false
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
