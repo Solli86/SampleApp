7e5dbaa7d1a07e2e8591634d212b3e0ef57b61aa
Solli86 <paul-sart@yandex.ru>
2015-06-30 19:48:47 +0300

    add destroy action

--- spec/features/micropost_pages_spec.rb
+++ spec/features/micropost_pages_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 describe "Micropost pages" do
 
   subject { page }
@@ -27,6 +28,17 @@ describe "Micropost pages" do
       before { fill_in 'micropost_content', with: "Lorem ipsum" }
       it "should create a micropost" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
+      end
+    end
+  end
+  describe "Micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it 'should delete a micropost' do
+        expect { click_link "delete" }.to change(Micropost, :count).by(-1) 
       end
     end
   end
